7eda2fefce6b3093a9aa09657e6d1108c36f35e9
kyrptonaught <anthony.butillo10@gmail.com>
2019-12-17 02:06:38 -0500

    Rewrite-ish for dedicated servers

--- src/main/java/net/kyrptonaught/coloredcampfire/ColoredCampfireMod.java
+++ src/main/java/net/kyrptonaught/coloredcampfire/ColoredCampfireMod.java
@@ -1,30 +1,27 @@
 package net.kyrptonaught.coloredcampfire;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
-import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
-import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
-import net.minecraft.client.texture.SpriteAtlasTexture;
-import net.minecraft.particle.BlockStateParticleEffect;
-import net.minecraft.particle.ParticleType;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 import java.util.HashMap;
 
 
 public class ColoredCampfireMod implements ClientModInitializer {
     private static final String MOD_ID = "coloredcampfire";
-    static HashMap<Integer, float[]> COLOR_CACHE = new HashMap<>();
-    public static final ParticleType<BlockStateParticleEffect> COLOREDSMOKE = Registry.register(Registry.PARTICLE_TYPE, MOD_ID + ":coloredsmoke", FabricParticleTypes.complex(true, BlockStateParticleEffect.PARAMETERS_FACTORY));
+    public static HashMap<Integer, float[]> COLOR_CACHE = new HashMap<>();
 
     @Override
     public void onInitializeClient() {
-        ClientSpriteRegistryCallback.event(SpriteAtlasTexture.PARTICLE_ATLAS_TEX).register((atlasTexture, registry) -> {
-            for (int i = 0; i < 12; i++) {
-                registry.register(new Identifier(MOD_ID, "big_smoke_" + i));
-            }
-        });
-        ParticleFactoryRegistry.getInstance().register(COLOREDSMOKE, ColoredSmokeFactory::new);
+
+    }
+
+    public static float[] getColorForBlock(int color) {
+
+        if (!COLOR_CACHE.containsKey(color)) {
+            int l = (color & 16711680) >> 16;
+            int m = (color & '\uff00') >> 8;
+            int n = (color & 255);
+            COLOR_CACHE.put(color, new float[]{(float) l / 255.0F, (float) m / 255.0F, (float) n / 255.0F});
+        }
+        return COLOR_CACHE.get(color);
     }
 }
---
+++ src/main/java/net/kyrptonaught/coloredcampfire/mixin/MixinCampfire.java
@@ -0,0 +1,25 @@
+package net.kyrptonaught.coloredcampfire.mixin;
+
+import net.minecraft.block.CampfireBlock;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Random;
+
+@Mixin(CampfireBlock.class)
+public class MixinCampfire {
+
+    @Inject(method = "spawnSmokeParticle(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;ZZ)V", at = @At(value = "HEAD"), cancellable = true)
+    private static void spawnColoredParticle(World world, BlockPos pos, boolean isSignal, boolean lotsOfSmoke, CallbackInfo cbi) {
+        if (world.isReceivingRedstonePower(pos)) {
+            Random random = world.getRandom();
+            world.addImportantParticle(ParticleTypes.CAMPFIRE_SIGNAL_SMOKE, true, (double) pos.getX() + 0.5D + random.nextDouble() / 3.0D * (double) (random.nextBoolean() ? 1 : -1), (double) pos.getY() + random.nextDouble() + random.nextDouble(), (double) pos.getZ() + 0.5D + random.nextDouble() / 3.0D * (double) (random.nextBoolean() ? 1 : -1), pos.getX(), pos.getY(), pos.getZ());
+            cbi.cancel();
+        }
+    }
+}
